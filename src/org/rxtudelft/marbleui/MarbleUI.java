f57434c6acff65f66347a2d5ddab2130d75877e9
Jeff Smits <jeff.smits@gmail.com>
2014-05-01 16:36:39 +0200

    Start of MarbleUI implementation

---
+++ src/org/rxtudelft/marbleui/MarbleUI.java
@@ -0,0 +1,14 @@
+package org.rxtudelft.marbleui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ */
+public class MarbleUI extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        // I feel so useful having written this much: done for today! (jk)
+    }
+}
