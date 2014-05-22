f048352f826bd798ad6dd30d1f5208491f840f1e
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-05-22 13:41:43 +0200

    ObservableShape stub

---
+++ src/org/rxtudelft/marbleui/shape/ObservableShape.java
@@ -0,0 +1,26 @@
+package org.rxtudelft.marbleui.shape;
+
+import javafx.scene.shape.Shape;
+import org.rxtudelft.marbleui.diagram.MarbleModel;
+import rx.Observable;
+
+/**
+ * Created by ferdy on 5/22/14.
+ */
+public class ObservableShape extends Shape {
+
+    private Observable<MarbleModel> model;
+
+    public ObservableShape(Observable<MarbleModel> model) {
+        this.model = model;
+    }
+
+    public Observable<MarbleModel> getModel() {
+        return this.model;
+    }
+
+    @Override
+    public com.sun.javafx.geom.Shape impl_configShape() {
+        return null;
+    }
+}
