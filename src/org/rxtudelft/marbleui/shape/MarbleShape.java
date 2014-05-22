ec8827576d4b01f5f1fe2935e9921fdbb63a9d46
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-05-22 12:42:28 +0200

    Shape class for marbles

---
+++ src/org/rxtudelft/marbleui/shape/MarbleShape.java
@@ -0,0 +1,14 @@
+package org.rxtudelft.marbleui.shape;
+
+import javafx.scene.shape.Shape;
+
+/**
+ * Created by ferdy on 5/22/14.
+ */
+abstract public class MarbleShape extends Shape {
+
+    @Override
+    public com.sun.javafx.geom.Shape impl_configShape() {
+        return null;
+    }
+}
