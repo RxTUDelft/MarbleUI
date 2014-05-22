2ec26102c3b7e2e68dde69ff9d8fdab4b58eb187
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-05-22 12:54:39 +0200

    MarbleShape has a MarbleModel

--- src/org/rxtudelft/marbleui/shape/MarbleShape.java
+++ src/org/rxtudelft/marbleui/shape/MarbleShape.java
@@ -1,11 +1,21 @@
 package org.rxtudelft.marbleui.shape;
 
 import javafx.scene.shape.Shape;
+import org.rxtudelft.marbleui.diagram.MarbleModel;
 
 /**
  * Created by ferdy on 5/22/14.
  */
 abstract public class MarbleShape extends Shape {
+    public MarbleModel model;
+
+    public MarbleShape(MarbleModel model) {
+        this.model = model;
+    }
+
+    public MarbleModel getModel() {
+        return this.model;
+    }
 
     @Override
     public com.sun.javafx.geom.Shape impl_configShape() {
