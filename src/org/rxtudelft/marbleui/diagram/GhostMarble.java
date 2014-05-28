3d16663fab2b85a3e603ca620b5ccea54888d5a4
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-05-28 00:27:10 +0000

    GhostMarble doesn't need to know what obs it's on

--- src/org/rxtudelft/marbleui/diagram/GhostMarble.java
+++ src/org/rxtudelft/marbleui/diagram/GhostMarble.java
@@ -4,19 +4,13 @@ package org.rxtudelft.marbleui.diagram;
  * Created by jeff on 22-5-14.
  */
 public final class GhostMarble {
-    private int obs;
     private double x;
 
     public double getX() {
         return x;
     }
 
-    public int getObs() {
-        return obs;
-    }
-
-    public GhostMarble(int obs, double x) {
-        this.obs = obs;
+    public GhostMarble(double x) {
         this.x = x;
     }
 }
