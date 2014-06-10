212ac9505a12461b2c0339936e79ce1f9aa8bd84
Jeff Smits <jeff.smits@gmail.com>
2014-06-10 19:21:08 +0200

    Right, and I tested this whole technique with the TestSchedulerTryout thing

--- src/org/rxtudelft/marbleui/TestSchedulerTryout.java
+++ src/org/rxtudelft/marbleui/TestSchedulerTryout.java
@@ -4,6 +4,8 @@ import rx.Observable;
 import rx.schedulers.TestScheduler;
 import rx.subjects.TestSubject;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -15,12 +17,19 @@ public class TestSchedulerTryout {
         TestScheduler s = new TestScheduler();
         TestSubject<Long> ts = TestSubject.create(s);
 
-        Stream.iterate(0, (m) -> m + 1).limit(100).forEach((i) -> {
+        Stream.iterate(0, (m) -> m + 1).limit(10).forEach((i) -> {
             ts.onNext(Long.valueOf(i), (i+1) * 1000);
         });
+        ts.onCompleted(11 * 1000);
 
-        Observable<Integer> b = Observable.from(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
-        ts.zip(b, (Long c, Integer d) -> c + d).subscribe((r) -> System.out.println(s.now() + ": " + r));
-        s.advanceTimeBy(9, TimeUnit.SECONDS);
+        Observable<Integer> b = Observable.from(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
+        Observable<String> result = ts.zip(b, (Long c, Integer d) -> c + d).map(r -> s.now() + ": " + r);
+        List<String> list = new ArrayList<>();
+
+        result.subscribe(str -> list.add(str));
+
+        s.advanceTimeBy(11, TimeUnit.SECONDS);
+
+        list.stream().forEach(System.out::println);
     }
 }
