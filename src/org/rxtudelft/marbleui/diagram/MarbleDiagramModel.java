00f104977add7e10d44db398ad57de1d3f375748
Jeff Smits <jeff.smits@gmail.com>
2014-06-10 17:45:36 +0200

    Output marbles can now be calculated

--- src/org/rxtudelft/marbleui/diagram/MarbleDiagramModel.java
+++ src/org/rxtudelft/marbleui/diagram/MarbleDiagramModel.java
@@ -1,9 +1,15 @@
 package org.rxtudelft.marbleui.diagram;
 
 import org.rxtudelft.marbleui.diagram.initOperator.InitOperator;
+import rx.Observable;
+import rx.schedulers.TestScheduler;
+import rx.schedulers.Timestamped;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * Created by ferdy on 5/8/14.
@@ -17,5 +23,21 @@ public class MarbleDiagramModel {
         this.operator = operator;
         this.observables = observables;
         this.output = new ObservableModel(new HashMap<>());
+    }
+
+    public void calcOutput() {
+        TestScheduler ts = new TestScheduler();
+
+        List<Observable<MarbleModel>> inputs = observables.stream()
+                .map(o -> o.testSubject(ts)).collect(Collectors.<Observable<MarbleModel>>toList());
+
+        Observable<Timestamped<MarbleModel>> outputObs = operator.call(inputs).map(o -> new Timestamped<>(ts.now(), o));
+        ArrayList<Timestamped<MarbleModel>> list = new ArrayList<>();
+
+        ts.advanceTimeTo(ObservableModel.MAX_TIME, TimeUnit.MILLISECONDS);
+
+        outputObs.subscribe(o -> list.add(o));
+
+        output = new ObservableModel(list.stream().collect(Collectors.toMap(Timestamped::getTimestampMillis, Timestamped::getValue)));
     }
 }
