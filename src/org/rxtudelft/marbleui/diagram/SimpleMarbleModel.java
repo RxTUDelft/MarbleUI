ab65618c6765c1c1a109ecfaa2d7751c07f3fff1
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-06-15 23:10:07 +0000

    Generated EQ method for simpleMarbleModel

--- src/org/rxtudelft/marbleui/diagram/SimpleMarbleModel.java
+++ src/org/rxtudelft/marbleui/diagram/SimpleMarbleModel.java
@@ -24,4 +24,24 @@ public class SimpleMarbleModel extends MarbleModel{
     public Color getColor() {
         return color;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SimpleMarbleModel that = (SimpleMarbleModel) o;
+
+        if (num != that.num) return false;
+        if (color != null ? !color.equals(that.color) : that.color != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = num;
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        return result;
+    }
 }
