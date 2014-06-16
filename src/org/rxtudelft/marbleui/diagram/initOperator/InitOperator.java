939967ca9d9dd1750545d1badbab82505fe47ac6
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-06-16 02:51:00 +0000

    Note rename InitOperator

--- src/org/rxtudelft/marbleui/diagram/initOperator/InitOperator.java
+++ src/org/rxtudelft/marbleui/diagram/initOperator/InitOperator.java
@@ -9,5 +9,6 @@ import java.util.List;
 /**
  * Created by ferdy on 5/16/14.
  */
+//FIXME There must be a name for this type of indirection
 public interface InitOperator extends Func1<List<Observable<MarbleModel>>, Observable<MarbleModel>> {
 }
