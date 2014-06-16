9e0afdd158c22b0453db66c32040052c31017d56
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-06-16 02:17:54 +0000

    Commented test scheduler tryout

--- src/org/rxtudelft/marbleui/TestSchedulerTryout.java
+++ src/org/rxtudelft/marbleui/TestSchedulerTryout.java
@@ -14,22 +14,36 @@ import java.util.stream.Stream;
  */
 public class TestSchedulerTryout {
     public static void main(String[] args) {
+        //create a test scheduler s to control manually
         TestScheduler s = new TestScheduler();
+
+        //create a test subject to publish events to
         TestSubject<Long> ts = TestSubject.create(s);
 
+        //publish 0...10 on the test schedule 1 second apart from each other
         Stream.iterate(0, (m) -> m + 1).limit(10).forEach((i) -> {
             ts.onNext(Long.valueOf(i), (i+1) * 1000);
         });
+        //and complete on second 11
         ts.onCompleted(11 * 1000);
 
+        //create a list of 1's
         Observable<Integer> b = Observable.from(1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-        Observable<String> result = ts.zip(b, (Long c, Integer d) -> c + d).map(r -> s.now() + ": " + r);
+        //to increment the previously created numbers,
+        Observable<String> result = ts.zip(b, (Long c, Integer d) -> c + d)
+                //and project them to [time: incrementedNumber(c+d)]
+                .map(r -> s.now() + ": " + r);
+
+        //create a list to store these numbers in
         List<String> list = new ArrayList<>();
 
+        //subscribe to the result, write all to the list
         result.subscribe(str -> list.add(str));
 
+        //advance time untill all events should be past
         s.advanceTimeBy(11, TimeUnit.SECONDS);
 
+        //print all numbers
         list.stream().forEach(System.out::println);
     }
 }
