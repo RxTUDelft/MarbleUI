764333e38871245097661b208a66be5c9d306e04
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-06-17 19:01:25 +0200

    Output calculated correctly

--- src/org/rxtudelft/marbleui/diagram/MarbleDiagramModel.java
+++ src/org/rxtudelft/marbleui/diagram/MarbleDiagramModel.java
@@ -38,19 +38,22 @@ public class MarbleDiagramModel {
 
         //create list of input rxObservables
         List<Observable<MarbleModel>> inputs = this.inputs.stream()
-                .map(o -> o.testSubject(ts)).collect(Collectors.<Observable<MarbleModel>>toList());
-
+                .map(o -> o.testSubject(ts)).collect(Collectors.<Observable<MarbleModel>>toList());;
+        
         //calculate timestamped output
-        Observable<Timestamped<MarbleModel>> outputObs = operator.call(inputs).timestamp();
+        Observable<Timestamped<MarbleModel>> outputObs = operator.call(inputs).map(marble -> {
+            return new Timestamped<MarbleModel>(ts.now(), marble);
+        });
+        //outputObs.subscribe(System.out::println, System.err::println);
         ArrayList<Timestamped<MarbleModel>> list = new ArrayList<>();
 
         //advance time past to max input time
-        ts.advanceTimeTo(ObservableModel.MAX_TIME, TimeUnit.MILLISECONDS);
 
         //put all marbles on output
         outputObs.subscribe(o -> {
             MarbleDiagramModel.this.output.put(o.getTimestampMillis(), o.getValue());
         });
+        ts.advanceTimeTo(ObservableModel.MAX_TIME, TimeUnit.MILLISECONDS);
     }
 
     public List<ObservableModel> getInputs() {
