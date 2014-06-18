8212ac642176d9f64671648f0be9c6bc5a57aa05
Ferdy Moon Soo Beekmans <ferdymoonsoobeekmans@gmail.com>
2014-06-18 18:57:42 +0200

    GroupByTryout

--- src/org/rxtudelft/marbleui/GroupByTryout.java
+++ src/org/rxtudelft/marbleui/GroupByTryout.java
@@ -1,7 +1,22 @@
 package org.rxtudelft.marbleui;
 
+import rx.Observable;
+import rx.observables.GroupedObservable;
+
+import java.lang.Boolean;
+import java.lang.Integer;
+
 /**
  * Created by ferdy on 6/18/14.
  */
 public class GroupByTryout {
+    public static void main(String[] args) {
+        Observable<GroupedObservable<Boolean,Integer>> grouped = Observable.from(0, 1, 2, 3).groupBy(i -> {
+            return i % 2 == 0;
+        });
+
+        grouped.subscribe(g -> {
+            System.out.println(g.getKey());
+        });
+    }
 }
