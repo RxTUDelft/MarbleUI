f2125b77a89dd9eb74b5aafedab251cf3ab7a555
Jeff Smits <jeff.smits@gmail.com>
2014-06-25 12:00:48 +0200

    Fixed padding of slider

--- src/org/rxtudelft/marbleui/view/Slider.java
+++ src/org/rxtudelft/marbleui/view/Slider.java
@@ -1,12 +1,7 @@
 package org.rxtudelft.marbleui.view;
 
 import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.value.ObservableDoubleValue;
-import javafx.beans.value.ObservableFloatValue;
-import javafx.beans.value.ObservableValue;
 import javafx.scene.Group;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
@@ -21,37 +16,41 @@ import rx.observables.JavaFxObservable;
 public class Slider extends Group {
     private DoubleProperty p;
     private Color c;
+    public static final int circleRadius = 10;
+    public static final int strokeWidth = 2;
+    public static final int pad = circleRadius + strokeWidth;
 
     public Slider(double w, double h, Color c) {
-        Rectangle bg = new Rectangle(w, h);
+        Rectangle bg = new Rectangle(w + 2 * pad, h);
         bg.setFill(Color.TRANSPARENT);
         this.getChildren().add(bg);
 
         Line hLine = new Line(0, h/2, w, h/2);
-        hLine.setStrokeWidth(2);
+        hLine.setTranslateX(pad);
+        hLine.setStrokeWidth(strokeWidth);
         hLine.setStroke(Color.BLACK);
         this.getChildren().add(hLine);
 
         this.p = new SimpleDoubleProperty(0.5);
 
-        Circle slider = new Circle(10);
-        slider.setStrokeWidth(2);
+        Circle slider = new Circle(circleRadius);
+        slider.setStrokeWidth(strokeWidth);
         slider.setStroke(Color.BLACK);
         slider.setFill(c);
-        slider.setTranslateX(w/2);
+        slider.setTranslateX(pad + w/2);
         slider.setTranslateY(h/2);
         this.getChildren().add(slider);
 
         JavaFxObservable.fromObservableValue(this.p).subscribe(p -> {
-            slider.setTranslateX(w * this.limitP(p.doubleValue()));
+            slider.setTranslateX(pad + w * Slider.limitP(p.doubleValue()));
         });
 
         JavaFxObservable.fromNodeEvents(this, MouseEvent.MOUSE_DRAGGED).subscribe(e -> {
-            this.p.setValue(this.limitP(e.getX() / w));
+            this.p.setValue(Slider.limitP((e.getX()-pad) / w));
         });
     }
 
-    private double limitP(double p) {
+    private static double limitP(double p) {
         if (p < 0) {
             return 0;
         }
